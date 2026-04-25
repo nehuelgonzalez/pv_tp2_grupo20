d12d5cc2f9f7c94a12bb571a63cc00ff7d69b2fa
Federico Agustin Salinas <fedeasalinas@gmail.com>
2026-04-25 00:40:57 -0300

    Se agrega resolución de ejercicios 2,9 y 11

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,1 +1,13 @@
-//Ejercicio2
+let mostrarImpares = () => {
+
+    const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 != 0) {
+            console.log(`El número ${numeros[i]} es impar`);
+        }
+    }
+
+}
+
+mostrarImpares();
