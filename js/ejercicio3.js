6c2a8026d0836d311a70628c295b499594dfd803
Joaquín nicolás Ibañez chicchi <ibanezjoaquin024@gmail.com>
2026-04-25 19:43:40 -0300

    resolucion de ejercicio 3,5,6,7

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,1 +1,4 @@
-//Ejercicio3
+//Ejercicio3   Calcular y mostrar por consola el volumen de una esfera cuyo radio = 4 m 
+const radio=4;
+const volumen=(4/3)*Math.PI*Math.pow(radio,3)
+console.log(`El volumen de la esfera es: ${volumen.toFixed(2)}`);
--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,1 +1,8 @@
-//Ejercicio5
+/*Ejercicio5 Declarar dos variables denominadas horas, minutos, asignar valores a cada una y 
+realizar la conversión para expresar esas horas y minutos en segundos. Mostrar por consola 
+las horas y minutos, luego el resultado de la conversión en segundos. */
+let horas=10;
+let minutos=12;
+console.log(`la hora ${horas} convertida en segundos es:: ${horas *3600}`);
+console.log(`los minutos ${minutos} convertidos en segundos son ${minutos*60
+}`);
